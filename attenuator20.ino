2ff903b3c887a9c614fc227b170dc043543128a1
gtnz <gtnzgtmz@gmail.com>
2022-06-27 16:15:35 +0300

    Add files via upload
    Обнаружение импульса

--- attenuator20.ino
+++ attenuator20.ino
@@ -10,16 +10,17 @@
 #define STEP              25
 #define IN_ROUND          1000
 #define BUTTON_SET_PIN    12
-#define DEOD_PIN          A6
+#define DEOD_PIN          7
 #define MIRROR            1
+#define IN_SEC            4000
 
 // NewEncoder encoder(ENCODER_PINA, ENCODER_PINB, 0, 10000, 0, FULL_PULSE); 
-U8G2_ST7565_ERC12864_1_4W_SW_SPI u8g2(U8G2_R0, /* scl=*/ 5, /* si=*/ 11, /* cs=*/ 10, /* rs=*/ 9, /* rse=*/ 8);
+U8G2_ST7565_ERC12864_1_4W_SW_SPI u8g2(U8G2_R0, /* scl=*/ 11, /* si=*/ 12, /* cs=*/ 8, /* rs=*/10 , /* rse=*/9 );
 
 uint16_t e, cof_save;
 uint8_t control = 0;
 bool set_button=false;
-float cof, e_final;
+float cof, e_final, zero;
 
 // Для энкодера
 #define btn_long_push 1000   // Длительность долинного нажатия кнопки
@@ -45,14 +46,24 @@ void setup(void) {
 
   PCICR =  0b00000010; // PCICR |= (1<<PCIE1); Включить прерывание PCINT1
   PCMSK1 = 0b00001110; // Разрешить прерывание для  A1, A2, A3
+  Serial.println("start!");
+  // Подсчет нуля
+  for (int i=0; i <= 8000; i++) {
+      zero = zero+analogRead(DEOD_PIN);
+  }
+    zero = zero/8000-2;
+    Serial.println(zero);
 }
  
 void loop(void) {
-  uint16_t read_e;
+  float cof, e_final_1, e_summ;
+  int sec[4000];
+  uint16_t read_e, endpulse=0;
+  uint32_t pulse = 0;
   char print_number[5]="";
   String print_line_2="";
   String print_line_1="";
-  delay(100);
+//  delay(100);
   if (digitalRead(BUTTON_SET_PIN)) { // если кнопка управления нажата
     if (!set_button){ // была ли нажата кнопка управления в прошлом такте
       set_button=!set_button;
@@ -76,17 +87,34 @@ void loop(void) {
     print_line_2=String("МДж/cм");
     print_line_1="2";
     cof = 1.0*cof_save*STEP/IN_ROUND;
-    e = analogRead(6);
-    for (int i=0; i <= 20; i++) {
-      if (e < analogRead(6)) {
-        e = analogRead(6);
-      }
-      delay(100);
+    e_summ=0;
+    Serial.println(e_summ);
+    for (int i=0; i <= IN_SEC; i++) {
+      sec[i] = analogRead(DEOD_PIN);
     }
+    pulse = 0;
+    for (int i=0; i <=IN_SEC; i++) {
+      if (sec[i]>zero*1.25){            // обнаружение импульса 
+        endpulse=i+100; 
+        while (sec[i]>zero*1.25) {
+          pulse = pulse + sec[i];
+          i=i+1;
+        }
+      e_summ=e_summ+pulse/100;
+      }
+    }
+    Serial.println(e_summ);
     Serial.println(e);
-    e = e/MIRROR;
-    e_final = e*cof;
-    dtostrf(e_final, 1, 3, print_number);
+    e = 1.0*e/MIRROR;
+    e_final_1 = 1.0*e_summ/16000-zero; // для настройки 
+    if (abs(e_final_1-e_final)>0) {
+      e_final = e_final_1; // для настройки 
+//      e_final = e_summ*cof;
+    }
+    if (e_final<3) {
+      e_final = 0;
+    }
+    dtostrf(e_final, 1, 1, print_number);
   }
   u8g2.firstPage();
   do {
